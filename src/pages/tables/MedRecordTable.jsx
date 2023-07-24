638343c7d6eb5cfdf302a562575f0fb37d76d892
Meet Ashok Dodiya <120380073+MeetDOD@users.noreply.github.com>
2023-07-24 14:15:43 +0530

    Update MedRecordTable.jsx

--- src/pages/tables/MedRecordTable.jsx
+++ src/pages/tables/MedRecordTable.jsx
@@ -8,7 +8,7 @@ import Swal from "sweetalert2";
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
 
 const MedRecordTable = () => {
-  const API = "http://localhost:5000/api/v1/med_records";
+  const API = "https://health-record.onrender.com/api/v1/med_records";
 
   const navigate = useNavigate();
   const [docs, setDocs] = useState([]);
@@ -53,7 +53,7 @@ const MedRecordTable = () => {
   };
 
   const handleDelete = (id) => {
-    axios.delete('http://localhost:5000/api/v1/med_records/'+id)
+    axios.delete('https://health-record.onrender.com/api/v1/med_records/'+id)
     .then(res =>{
       Swal.fire({
         title:'Record Deleted',
